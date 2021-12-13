1d146c6f37af1935536ce8706c2ec863c7c91e93
Sema Kırdar <sema@dijitaladam.com>
2021-12-13 15:55:02 +0300

    recipes migration created

---
+++ database/migrations/2021_12_13_122557_create_recipes_table.php
@@ -0,0 +1,39 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateRecipesTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('recipes', function (Blueprint $table) {
+            $table->id();
+            $table->unsignedBigInteger('category_id');
+            $table->unsignedBigInteger('user_id');
+            $table->string('name');
+            $table->string('prepare_time');
+            $table->string('cooking_time');
+            $table->integer('serving');
+            $table->longText('description');
+
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('recipes');
+    }
+}
