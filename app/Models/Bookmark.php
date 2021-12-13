c6e35a55ba26b8e4cb26b02ce7f795dff62808b0
Sema Kırdar <sema@dijitaladam.com>
2021-12-13 16:55:31 +0300

    bookmark model created

---
+++ app/Models/Bookmark.php
@@ -0,0 +1,15 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+
+class Bookmark extends Model
+{
+    use HasFactory;
+    protected $fillable = [
+        'user_id',
+        'recipe_id'
+    ];
+}
