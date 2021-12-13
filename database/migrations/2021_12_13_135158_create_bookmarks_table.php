68fd6a2f0670bb3c70614ee9a0decfe98ad6933e
Sema Kırdar <sema@dijitaladam.com>
2021-12-13 16:52:18 +0300

    bookmark migration created

--- database/migrations/2021_12_13_135158_create_bookmarks_table.php
+++ database/migrations/2021_12_13_135158_create_bookmarks_table.php
@@ -4,7 +4,7 @@ use Illuminate\Database\Migrations\Migration;
 use Illuminate\Database\Schema\Blueprint;
 use Illuminate\Support\Facades\Schema;
 
-class CreateBookmarkTable extends Migration
+class CreateBookmarksTable extends Migration
 {
     /**
      * Run the migrations.
@@ -13,14 +13,9 @@ class CreateBookmarkTable extends Migration
      */
     public function up()
     {
-        Schema::create('bookmark', function (Blueprint $table) {
+        Schema::create('bookmarks', function (Blueprint $table) {
             $table->id();
-            $table->unsignedBigInteger('user_id');
-            $table->unsignedBigInteger('recipe_id');
             $table->timestamps();
-
-            $table->foreign('user_id')->references('id')->on('users');
-            $table->foreign('recipe_id')->references('id')->on('recipes');
         });
     }
 
@@ -31,6 +26,6 @@ class CreateBookmarkTable extends Migration
      */
     public function down()
     {
-        Schema::dropIfExists('bookmark');
+        Schema::dropIfExists('bookmarks');
     }
 }
